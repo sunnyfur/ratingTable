d662a00ac8ee4cc1150ca7870dde58269f7e44a4
sunnyfur <polin.makarova@mail.ru>
2022-11-28 16:04:58 +0500

    feat: add classes for sort

--- assets/js/drawTableHtml.js
+++ assets/js/drawTableHtml.js
@@ -1,59 +1,68 @@
 const drawTableRow = (person, place) => {
   const cells = [
-    { label: "Место", data: place },
-    { label: "ФИО", data: person.name },
-    { label: "Город", data: person.city },
-    { label: "Машина", data: person.car },
-    { label: "Общая сумма баллов", data: person.summ.toString() },
-    { label: "Первый заезд", data: person.results[0].toString() },
-    { label: "Второй заезд", data: person.results[1].toString() },
-    { label: "Третий заезд", data: person.results[2].toString() },
-    { label: "Четвертый заезд", data: person.results[3].toString() },
+    { label: "Место", data: place, class: "table__data" },
+    { label: "ФИО", data: person.name, class: "table__data" },
+    { label: "Город", data: person.city, class: "table__data" },
+    { label: "Машина", data: person.car, class: "table__data" },
+    {
+      label: "Общая сумма баллов",
+      data: person.summ.toString(),
+      class: "table__data data_racesumm",
+    },
+    {
+      label: "Первый заезд",
+      data: person.results[0].toString(),
+      class: "table__data data_race0",
+    },
+    {
+      label: "Второй заезд",
+      data: person.results[1].toString(),
+      class: "table__data data_race1",
+    },
+    {
+      label: "Третий заезд",
+      data: person.results[2].toString(),
+      class: "table__data data_race2",
+    },
+    {
+      label: "Четвертый заезд",
+      data: person.results[3].toString(),
+      class: "table__data data_race3",
+    },
   ];
 
   const row = createElemDOM("tr", "table__row");
 
   cells.forEach((cell) => {
-    let cellHTML = createElemDOM("td", "table__data", cell.data);
+    let cellHTML = createElemDOM("td", cell.class, cell.data);
     row.appendChild(cellHTML);
     cellHTML.dataset.label = cell.label;
   });
-
-  // let cell = createElemDOM("td", "table__data", place);
-  // row.appendChild(cell);
-  // cell.dataSet.cell = "Место";
-  // row.appendChild(createElemDOM("td", "table__data", person.name));
-  // row.appendChild(createElemDOM("td", "table__data", person.city));
-  // row.appendChild(createElemDOM("td", "table__data", person.car));
-  // row.appendChild(createElemDOM("td", "table__data", person.summ.toString()));
-  // row.appendChild(
-  //   createElemDOM("td", "table__data", person.results[0].toString())
-  // );
-  // row.appendChild(
-  //   createElemDOM("td", "table__data", person.results[1].toString())
-  // );
-  // row.appendChild(
-  //   createElemDOM("td", "table__data", person.results[2].toString())
-  // );
-  // row.appendChild(
-  //   createElemDOM("td", "table__data", person.results[3].toString())
-  // );
   return row;
+};
+
+const sortTable = (pos) => {
+  const table = document.querySelector(".table__body");
+  table.innerHTML = "";
+  let cells = document.querySelectorAll(".active");
+  cells.forEach((cell) => cell.classList.toggle("active"));
+
+  if (+pos) {
+    [...SortClass.getSortRace(+pos)].forEach((person, index) =>
+      table.appendChild(drawTableRow(person, index + 1))
+    );
+  } else {
+    [...SortClass.getSortSum()].forEach((person, index) =>
+      table.appendChild(drawTableRow(person, index + 1))
+    );
+  }
+  cells = document.querySelectorAll(`.data_race${pos}`);
+  cells.forEach((cell) => cell.classList.toggle("active"));
 };
 
 const handleChange = (e) => {
   if (e.target.checked) {
-    const table = document.querySelector("tbody");
-    table.innerHTML = "";
-    if (+e.target.value) {
-      [...SortClass.getSortRace(+e.target.value)].forEach((person, index) =>
-        table.appendChild(drawTableRow(person, index + 1))
-      );
-    } else {
-      [...SortClass.getSortSum()].forEach((person, index) =>
-        table.appendChild(drawTableRow(person, index + 1))
-      );
-    }
+    sortTable(e.target.value);
   }
 };
 
@@ -61,9 +70,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   await SortClass.Init();
 
   const table = document.querySelector("tbody");
-  [...SortClass.getSortSum()].forEach((person, index) =>
-    table.appendChild(drawTableRow(person, index + 1))
-  );
+  sortTable("summ");
+
   document.querySelectorAll(".race__input").forEach((input) => {
     input.addEventListener("change", handleChange);
   });
